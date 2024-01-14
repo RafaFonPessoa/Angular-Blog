d06400ef58dcc6f279117a9a868c4149055eee5d
RafaFonPessoa <rafaelpessoabr@gmail.com>
2024-01-13 23:29:37 -0300

    Projeto Finalizado

---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  templateUrl: './small-card.component.html',
+  styleUrl: './small-card.component.css'
+})
+export class SmallCardComponent {
+  @Input()
+  photoCover:string = ""
+  @Input()
+  cardTitle:string = ""
+  @Input()
+  id:string = "0"
+}
